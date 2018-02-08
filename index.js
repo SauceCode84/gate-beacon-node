408da0b512ea532999c4260419b0a6fb1e2a801e
Richard Ueckermann <rueckermann@sse.co.za>
2018-02-08 10:09:02 +0200

    example with eddystone beacon

---
+++ index.js
@@ -0,0 +1,25 @@
+const bleno = require("bleno");
+const beacon = require("eddystone-beacon");
+
+let char = new bleno.Characteristic({
+  uuid: "E0D38F1C-56CA-4B75-9D44-3E4134F7CB0B",
+  properties: ["read"],
+  value: new Buffer("example")
+});
+
+let service = new bleno.PrimaryService({
+  uuid: 'E0D38F1C-56CA-4B75-9D44-3E4134F7CB0A',
+  characteristics: [ characteristic ]
+});
+
+bleno.once("advertisingStart", (err) => {
+  if (err) {
+    throw err;
+  }
+
+  console.log("on -> advertisingStart");
+
+  bleno.setServices([ service ]);
+});
+
+beacon.advertiseUrl("https://goo.gl/pheJrb");
